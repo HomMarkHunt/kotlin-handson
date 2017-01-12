04f047dec9ecccc02c90033bde7d6a025ac0f023
HomMarkHunt <honmaso@gmail.com>
2017-01-13 07:02:43 +0900

    create handson ほにゃらら.

---
+++ src/main/kotlin/Handson1.kt
@@ -0,0 +1,29 @@
+/**
+ * ハンズオン1 Hello World.
+ *
+ * ---
+ * Step1 ~Hello World~
+ *
+ * Hint: mainメソッドの宣言 -> [main + Tab]
+ * Hint: 標準出力 -> println()
+ *
+ * ---
+ * Step2 ~変数の宣言~
+ *
+ * Hint: kotlinのデータ型はval, var
+ *       Valはimmutable(再代入不可)
+ *       Varはmutable(再代入可)
+ *
+ * ---
+ * Step3 ~型の宣言~
+ * Hint: 型宣言は後置
+ *       val text: String
+ *
+ * ---
+ * Step4 ~時間が余った人は~
+ * Hint: Stringがデファルトで持ってるメソッドをIDE補完で見てみよう！
+ *
+ */
+fun main(args: Array<String>) {
+    // TODO
+}
